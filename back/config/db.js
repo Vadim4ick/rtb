557b262525c7d102ecc1978f78d3b8720ad121dc
Vadim Firulyov <firulvv@mail.ru>
2023-01-19 15:33:44 +0500

    update prev

--- back/config/db.js
+++ back/config/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDb = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    // const conn = await mongoose.connect("mongodb://127.0.0.1:27017/workout");
+    // const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://127.0.0.1:27017/workout");
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
--- back/controllers/exercise/log/updateController.js
+++ back/controllers/exercise/log/updateController.js
@@ -1,55 +1,54 @@
-import asyncHandler from "express-async-handler";
-import ExerciseLog from "../../../models/exerciseLogModel.js";
+import asyncHandler from 'express-async-handler'
+import ExerciseLog from '../../../models/exerciseLogModel.js'
 
-//@desc   Update exercisesLog
-//@route  PUT /api/exercises/log
-//@access Private
-
+// @desc    Update exercise log
+// @route   PUT /api/exercises/log
+// @access  Private
 export const updateExerciseLog = asyncHandler(async (req, res) => {
-  const { logId, timeIndex, key, value } = req.body;
+	const { logId, timeIndex, key, value } = req.body
 
-  const currentLog = await ExerciseLog.findById(logId);
+	const currentLog = await ExerciseLog.findById(logId)
 
-  if (!currentLog) {
-    res.status(404);
-    throw new Error("Данный лог не найден");
-  }
+	if (!currentLog) {
+		res.status(404)
+		throw new Error('Данный лог не найден!')
+	}
 
-  let newTimes = currentLog.times;
+	let newTimes = currentLog.times
 
-  if ((!timeIndex && timeIndex !== 0) || !key || (!value && value !== false)) {
-    res.status(404);
-    throw new Error("Вы не указали все поля!");
-  }
+	if ((!timeIndex && timeIndex !== 0) || !key || (!value && value !== false)) {
+		res.status(404)
+		throw new Error('Вы не указали все поля!')
+	}
 
-  newTimes[timeIndex][key] = value;
-  currentLog.time = newTimes;
+	newTimes[timeIndex][key] = value
 
-  const updatedLog = await currentLog.save();
+	currentLog.times = newTimes
 
-  res.json(updatedLog);
-});
+	const updatedLog = await currentLog.save()
 
-//@desc   Update status of complete exercise log
-//@route  PUT /api/exercises/log/completed
-//@access Private
+	res.json(updatedLog)
+})
 
+// @desc    Update status of complete exercise log
+// @route   PUT /api/exercises/log/complete
+// @access  Private
 export const updateCompleteExerciseLog = asyncHandler(async (req, res) => {
-  const { logId, completed } = req.body;
+	const { logId, completed } = req.body
 
-  const currentLog = await ExerciseLog.findById(logId).populate(
-    "exercise",
-    "workout"
-  );
+	const currentLog = await ExerciseLog.findById(logId).populate(
+		'exercise',
+		'workout'
+	)
 
-  if (!currentLog) {
-    res.status(404);
-    throw new Error("Данный лог не найден");
-  }
+	if (!currentLog) {
+		res.status(404)
+		throw new Error('Данный лог не найден!')
+	}
 
-  currentLog.completed = completed;
+	currentLog.completed = completed
 
-  const updatedLog = await currentLog.save();
+	const updatedLog = await currentLog.save()
 
-  res.json(updatedLog);
-});
+	res.json(updatedLog)
+})
